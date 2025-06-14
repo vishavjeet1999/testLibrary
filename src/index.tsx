9a3e3efccbf188b1db39c4b4848d480643f75f0d
Vishavjeet1999 <vishavjeetsharma88@gmail.com>
2025-06-14 16:13:26 +0530

    new method

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,7 @@ import AwesomeLibrary from './NativeAwesomeLibrary';
 export function multiply(a: number, b: number): number {
   return AwesomeLibrary.multiply(a, b);
 }
+
+export function square(a: number): number {
+  return AwesomeLibrary.square(a, a);
+}
